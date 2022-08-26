d97e7d2787ff4ee1f0f9fe7ea570dd59c4f7cefe
Tariq-Hennache <tariq.hennache@gmail.com>
2022-08-26 09:31:13 +0100

    MVP

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -5,6 +5,23 @@ public class ParenthesesChecker {
 
     public static boolean checkParentheses(String testString) {
 
+        int leftParentheses = 0;
+        int rightParentheses = 0;
+        for (Character character : testString.toCharArray()){
+            if (character == '('){
+                leftParentheses += 1;
+            } else if (character == ')' ){
+                rightParentheses += 1;
+            }else leftParentheses += 0;
+        }
+        if (leftParentheses == rightParentheses){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
 
 }
 
