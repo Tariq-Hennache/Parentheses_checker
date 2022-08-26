0df29db00313a8c29a4d8561adfc4b44582ed838
Tariq-Hennache <tariq.hennache@gmail.com>
2022-08-26 10:05:47 +0100

    extension complete apart from 1 test'

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -7,14 +7,32 @@ public class ParenthesesChecker {
 
         int leftParentheses = 0;
         int rightParentheses = 0;
+        int leftSquare = 0;
+        int rightSquare = 0;
+        int leftCurly = 0;
+        int rightCurly = 0;
+        int leftAngle = 0;
+        int rightAngle = 0;
         for (Character character : testString.toCharArray()){
             if (character == '('){
                 leftParentheses += 1;
+            } else if (character == '{' ){
+                leftCurly += 1;
+            } else if (character == '['){
+                leftSquare += 1;
+            } else if (character == '<'){
+                leftAngle += 1;
             } else if (character == ')' ){
                 rightParentheses += 1;
-            }else leftParentheses += 0;
+            } else if (character == ']'){
+                rightSquare += 1;
+            } else if (character == '}'){
+                rightCurly += 1;
+            } else if (character == '>'){
+                rightAngle  += 1;
+            } else leftParentheses += 0;
         }
-        if (leftParentheses == rightParentheses){
+        if (leftParentheses == rightParentheses && leftCurly == rightCurly && leftAngle == rightAngle && leftSquare == rightSquare){
             return true;
         } else {
             return false;
